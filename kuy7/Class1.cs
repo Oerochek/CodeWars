e76ebaf69654e30a41538bab07029880e7ee39e5
1 <1@DESKTOP-UKLJV7M>
2022-08-14 17:12:38 +0300

    Add project files.

---
+++ kuy7/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.kuy7
+{
+    internal class Class1
+    {
+
+    }
+}
