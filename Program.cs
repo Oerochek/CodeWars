b39970141d1b4c711efb0cabedcd36b89293df9d
Orochek <anonim5894466>
2022-08-16 17:43:15 +0300

    task 1

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ConsoleApp2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // The Three Parts of a LINQ Query:
+            // 1. Data source.
+            int[] numbers = new int[] {2,43,645,8474,33333,19};
+
+            Kata.Sum(numbers);
+
         }
     }
 }
---
+++ kuy7/sum_without_highest_and_lowest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int Sum(int[] numbers)
+    {
+        if (numbers == null || numbers.Length <= 1)
+        {
+            return 0;
+        }
+
+        return GetMaxAndMin(numbers);
+        
+    }
+
+    static int GetMaxAndMin(int[] array)
+    {
+        int max = array[0];
+        int min = array[0];
+        int summa = 0;
+
+        foreach (var number in array)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+
+            if (number < min)
+            {
+                min = number;
+            }
+            summa = summa + number;
+        }
+        
+        summa = summa - max - min;
+        return summa;
+    }
+}
