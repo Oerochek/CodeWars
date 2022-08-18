f70e66435d9d6b03e086abe0f33c455523d6bc6f
Orochek <anonim5894466>
2022-08-18 14:15:54 +0300

    7Kuy

---
+++ kuy7/Shortest Word.cs
@@ -0,0 +1,27 @@
+﻿//сори у меня закрылась предыдущая задача решил новую таково же уровня и да код не мой но у меня было тоже самое только я не мог вернуть длину и по запросу в гугле выдало это
+/*
+ Simple, given a string of words, return the length of the shortest word(s).
+
+ String will never be empty and you do not need to account for different data types.
+*/
+
+using System.Linq;
+public class Kata
+{
+    public static int FindShort(string str)
+    {
+        string min = str.Split().OrderBy(s => s.Length).FirstOrDefault();
+        return min.Length;
+    }
+}
+
+/*
+ using System.Linq;
+ public class Kata
+ {
+   public static int FindShort(string s)
+   {
+     return s.Split(' ').Min(x => x.Length);
+   }
+ }
+*/
