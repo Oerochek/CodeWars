471a17458ab12647e6a07e075b35e867c45d1eac
Orochek <anonim5894466>
2022-08-22 21:56:25 +0300

    tests

---
+++ Kuy8/Removing Elements.cs
@@ -0,0 +1,45 @@
+﻿/*
+ Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+ Example:
+ ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+ None of the arrays will be empty, so you don't have to worry about that!
+*/
+public static class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+
+        object[] result;
+        if (arr.Length % 2 != 0)
+        {
+            result = new object[arr.Length / 2 + 1];
+        }
+        else
+        {
+            result = new object[arr.Length / 2];
+        }
+
+        for (int i = 0, j = 0; i < arr.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                result[j] = arr[i];
+                j++;
+            }
+        }
+        return result;
+    }
+}
+/*
+ using System.Linq;
+
+ public static class Kata
+ {
+     public static object[] RemoveEveryOther(object[] arr)
+     {
+         return arr.Where((e, i) => i%2 == 0).ToArray();
+     }
+ }
+*/
---
+++ Kuy8/Returning Strings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//супер просто
+/*
+ Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+ [Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+
+public static class Kata
+{
+    public static string Greet(string name)
+    {
+        return $"Hello, {name} how are you doing today?";
+    }
+}
+
+/*
+ using System;
+
+ public static class Kata
+ {
+   public static string Greet(string name) => $"Hello, {name} how are you doing today?";
+ }
+*/
