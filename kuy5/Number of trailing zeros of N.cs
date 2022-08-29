134987c7918436d4ceccf0596bf49305edf09f2a
Orochek <anonim5894466>
2022-08-29 13:41:30 +0300

    ура

---
+++ kuy5/Number of trailing zeros of N.cs
@@ -0,0 +1,42 @@
+﻿using System;
+/*
+ * вот тут пришлось попотеть с поиском инфы про подобные матемотические задачи в реальной жизни
+
+ Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+ N! = 1 * 2 * 3 *  ... * N
+
+ Be careful 1000! has 2568 digits...
+
+ For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+ Examples
+ zeros(6) = 1
+ # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+ zeros(12) = 2
+ # 12! = 479001600 --> 2 trailing zeros
+ Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.
+*/
+public static class Kata
+{
+    public static int TrailingZeros(int num)
+    {
+        int zeros = 0;
+        while (num > 0)
+        {
+            num /= 5;
+            zeros += num;
+        }
+        return zeros;
+    }
+}
+/*
+ using System;
+ using System.Linq;
+
+ public static class Kata 
+ {
+   public static int TrailingZeros(int n) => Enumerable.Range(1, (int)Math.Log(n, 5)).Sum(i => (int)(n / Math.Pow(5, i)));
+ }
+*/
---
+++ kuy5/What's a Perfect Power anyway.cs
@@ -0,0 +1,47 @@
+﻿using System;
+/*
+ A perfect power is a classification of positive integers:
+
+ In mathematics, a perfect power is a positive integer that can be expressed as an integer power of another positive integer. More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that mk = n.
+
+ Your task is to check wheter a given integer is a perfect power. If it is a perfect power, return a pair m and k with mk = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+
+ Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2, so (3,4) and (9,2) are valid solutions. However, the tests take care of this, so if a number is a perfect power, return any pair that proves it.
+
+ Examples
+ IsPerfectPower(4) => (2,2)
+ IsPerfectPower(5) => null
+ IsPerfectPower(8) => (2,3)
+ IsPerfectPower(9) => (3,2)
+*/
+public class PerfectPower
+{
+    public static (int, int)? IsPerfectPower(int n)
+    {
+        for (int i = 2; i * i <= n; i++)
+        {
+            for (int j = 2; Math.Pow(i, j) <= n; j++)
+            {
+                if (Math.Pow(i, j) == n)
+                {
+                    return (i, j);
+                }
+            }
+        }
+
+        return null;
+    }
+}
+/*
+ using System;
+ public class PerfectPower
+ {
+     public static (int, int)? IsPerfectPower(int n)
+     {
+        for (var m = 2; m * m <= n; ++m)
+             for (var k = 2; Math.Pow(m, k) <= n; ++k)
+             if (Math.Pow(m, k) == n) return (m, k);
+             return null;
+     }
+ }
+*/
